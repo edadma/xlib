1c132e6cf2a86c6f4eea09f32d66cc444f9fd56f
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-11 04:16:59 -0400

    cont coding basic functions and types

--- src/main/scala/io/github/edadma/xlib/extern/Xlib.scala
+++ src/main/scala/io/github/edadma/xlib/extern/Xlib.scala
@@ -6,8 +6,20 @@ import scala.scalanative.unsafe._
 @extern
 object Xlib {
 
-  type Display = Ptr[CStruct0]
-  type XEvent  = Ptr[CInt]
+  type XID      = CUnsignedLong
+  type Window   = XID
+  type Drawable = XID
+  type Font     = XID
+  type Pixmap   = XID
+  type Cursor   = XID
+  type Colormap = XID
+  type GContext = XID
+  type KeySym   = XID
+  type KeyCode  = CUnsignedChar
+  type Display  = Ptr[CStruct0]
+  type XEvent   = Ptr[CInt] //first field is an event type int
+  type Visual_s = CStruct0 //todo: xlib.h 227
+  type Visual   = Ptr[Visual_s]
 
   def XPending(display: Display): CInt = extern //2891
 
